d97093b50fc8ffe70d9dbc1e052eb80e5d97231e
Mustafa Bacanak <bacanak117@gmail.com>
2022-12-08 16:04:03 +0300

    project

--- src/main/java/ogrenci_veritabani_uygulamasi/Main.java
+++ src/main/java/ogrenci_veritabani_uygulamasi/Main.java
@@ -3,12 +3,13 @@ package ogrenci_veritabani_uygulamasi;
 public class Main {
 
     public static void main(String[] args) {
-
         OgrenciPojo ogr = new OgrenciPojo();
         ogr.ogrenciNoBelirle();
         ogr.dersKaydi();
-
-
-
+        ogr.odemeMitari();
+        System.out.println(OgrenciPojo.ANSI_YELLOW + "--------------------------");
+        System.out.println(OgrenciPojo.ANSI_GREEN + OgrenciPojo.ANSI_BOLD + "Kaydınız başarılı bir şekilde tamamlanmıştır."
+                + "\nDerslerinizi sorumlu danışmanınıza onaylatınız.");
     }
+
 }
--- src/main/java/ogrenci_veritabani_uygulamasi/Task.java
+++ src/main/java/ogrenci_veritabani_uygulamasi/Task.java
@@ -2,26 +2,42 @@ package ogrenci_veritabani_uygulamasi;
 
 public class Task {
 
-                // Pojo
+/*
+    Uygulamanızın aşağıdaki işlemleri yapması gerekiyor:
+    Kullanıcıya, veritabanına kaç öğrencinin ekleneceğini sormalı
+    Kullanıcı her öğrenci için isim, soy isim ve yıl (kaçıncı sınıf) seçeneği bilgilerini girmeli
+    Her öğrenci 5 haneli sayısal ve eşsiz bir öğrenci numarasına sahip olmalı ve
+    bu numaranın ilk sayısı, öğrencinin yıl bilgisi olmalı.
+    Öğrenciler aşağıdaki derslerden istediklerine kayıt olabilirler:
 
+        Tarih 101
+        Matematik 103
+        İngilizce 105
+        Kimya 107
+        Bilgisayar Bilimleri 109
+
+    Her ders için kayıt ücreti 600 TL olmalı
+    Öğrenciler dersleri seçtikten sonra toplam kurs ücretini görebilmeli ve kaç TL ödeme yapacağını yazabilmeli
+    Kayıt tamamlandıktan sonra ekranda öğrencilerin son durumlarını, aşağıdaki bilgileri içerecek şekilde görmeliyiz:
+
+    İsim Soyisim :
+    Öğrenci no :
+    Yıl :
+    Kayıt olunan dersler :
+    Kalan borç :
+*/
+
+    // Pojo
     // Variable’lar
-
     // Constructor: İsim, soyisim ve yıl bilgisini al
-
     // Öğrenci no oluştur
-
     // Derse kayıt ol
-
     // Toplam borcu görüntüle
-
     // Öğrenciye ne kadar ödeme yapacağını sor ve hesapla
-
     // Kayıttan sonra ekranda son durumu göster
 
 
-                 // Main
-
+    // Main
     // Kaç öğrenci eklemek istediğini sor
-
     // n sayısında yeni öğrenci oluştur
 }
